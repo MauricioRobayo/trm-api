79063233cc6d774325949da873407b5286f61cc9
Mauricio Robayo <rfmajo@gmail.com>
2020-08-11 13:13:09 -0500

    feat: add between endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import axios from 'axios';
-import { TrmApiQuote } from './types';
+
+export interface TrmApiQuote {
+  valor: string;
+  unidad: string;
+  vigenciadesde: string;
+  vigenciahasta: string;
+}
 
 class TrmApi {
   private trmApiUrl = 'https://www.datos.gov.co/resource/32sa-8pi3';
 
   async latest(): Promise<TrmApiQuote> {
-    const response = await axios.get(`${this.trmApiUrl}?$limit=1`);
-    return response.data;
+    const { data } = await axios.get<TrmApiQuote[]>(
+      `${this.trmApiUrl}?$limit=1`
+    );
+    return data[0];
+  }
+
+  async between({
+    startAt,
+    endAt,
+  }: {
+    startAt: string;
+    endAt: string;
+  }): Promise<TrmApiQuote[]> {
+    const searchString = `$where=vigenciadesde >= '${startAt}' AND vigenciahasta <= '${endAt}'`;
+    const { data } = await axios.get<TrmApiQuote[]>(
+      `${this.trmApiUrl}?${searchString}`
+    );
+    return data;
   }
 }
 
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface TrmApiQuote {
-  valor: string;
-  unidad: string;
-  vigenciadesde: string;
-  vigenciahasta: string;
-}
