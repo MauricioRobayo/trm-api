3a5fcef03e689861b3f169e04f4fe4412d6df097
Mauricio Robayo <rfmajo@gmail.com>
2023-07-30 09:35:04 -0500

    update deps (#716)
    * update deps
    
    * use pnpm instead of npm
    
    * Revert "use pnpm instead of npm"
    
    This reverts commit 2d6663aede9453996e31b1c5fede22d0a73533a7.
    
    * fix package issue

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
-  collectCoverageFrom: ["src/**/*"],
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
